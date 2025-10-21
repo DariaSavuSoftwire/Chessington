84b2d3bccb710f35490585cf1f78cbe8b4c1f0ec
DariaSavuSoftwire <Daria.Savu@softwire.com>
2025-10-21 14:15:06 +0300

    Add helper function

--- Chessington.GameEngine/Pieces/Pawn.cs
+++ Chessington.GameEngine/Pieces/Pawn.cs
@@ -5,14 +5,18 @@ namespace Chessington.GameEngine.Pieces
     public class Pawn : Piece
     {
         public Pawn(Player player)
-            : base(player) { }
+            : base(player){ }
+
+        public int GetStartingRow(Player player)
+        {
+            return player == Player.White ? GameSettings.BoardSize - 1 : 1;
+        }
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
             List<Square> moves = new List<Square>();
             Square currentSquare = board.FindPiece(this);
-            bool hasNotMovedBefore = Player == Player.White && currentSquare.Row == GameSettings.BoardSize - 1 ||
-                                     Player == Player.Black && currentSquare.Row == 1;
+            bool hasNotMovedBefore = currentSquare.Row == GetStartingRow(Player);
             for (int move = 1; move <= 2; move++)
             {
                 Square nextSquare = Player == Player.White
