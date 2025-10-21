2b22280a01f33e51c502c20ecfe0f611cc09b204
DariaSavuSoftwire <Daria.Savu@softwire.com>
2025-10-21 14:38:21 +0300

    Rook changes

--- Chessington.GameEngine/Pieces/Rook.cs
+++ Chessington.GameEngine/Pieces/Rook.cs
@@ -7,26 +7,45 @@ namespace Chessington.GameEngine.Pieces
     public class Rook : Piece
     {
         public Rook(Player player)
-            : base(player)
-        {
-        }
+            : base(player) { }
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
             Square currentSquare = board.FindPiece(this);
             List<Square> moves = new List<Square>();
-            for (int col = 0; col <GameSettings.BoardSize; col++)
+
+            for (int col = currentSquare.Col - 1; col >= 0; col--)
             {
                 Square nextSquare = Square.At(currentSquare.Row, col);
-                if(board.GetPiece(nextSquare) == null)
-                    moves.Add(nextSquare);
+                if (board.GetPiece(nextSquare) != null)
+                    break;
+                moves.Add(nextSquare);
             }
-            for (int row = 0; row <GameSettings.BoardSize; row++)
+
+            for (int row = currentSquare.Row + -1; row >= 0; row--)
             {
                 Square nextSquare = Square.At(row, currentSquare.Col);
-                if(board.GetPiece(nextSquare) == null)
-                    moves.Add(nextSquare);
+                if (board.GetPiece(nextSquare) != null)
+                    break;
+                moves.Add(nextSquare);
             }
+
+            for (int col = currentSquare.Col + 1; col < GameSettings.BoardSize; col++)
+            {
+                Square nextSquare = Square.At(currentSquare.Row, col);
+                if (board.GetPiece(nextSquare) != null)
+                    break;
+                moves.Add(nextSquare);
+            }
+
+            for (int row = currentSquare.Row + 1; row < GameSettings.BoardSize; row++)
+            {
+                Square nextSquare = Square.At(row, currentSquare.Col);
+                if (board.GetPiece(nextSquare) != null)
+                    break;
+                moves.Add(nextSquare);
+            }
+
             return moves;
         }
     }
