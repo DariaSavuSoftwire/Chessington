d48a75ae4fb286a48614c26fd2a81325a540ef09
DariaSavuSoftwire <Daria.Savu@softwire.com>
2025-10-21 14:23:00 +0300

    Move king

--- Chessington.GameEngine/Pieces/King.cs
+++ Chessington.GameEngine/Pieces/King.cs
@@ -5,12 +5,22 @@ namespace Chessington.GameEngine.Pieces
 {
     public class King : Piece
     {
+        private (int, int )[] kingMoves = { (-1, -1), (-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1) };
+
         public King(Player player)
             : base(player) { }
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            List<Square> moves = new List<Square>();
+            Square currentSquare = board.FindPiece(this);
+            foreach (var (row, col) in kingMoves)
+            {
+                Square nextSquare = Square.At(currentSquare.Row + row, currentSquare.Col + col);
+                if (board.IsSquareInBoard(nextSquare) && board.GetPiece(nextSquare) == null)
+                    moves.Add(nextSquare);
+            }
+            return moves;
         }
     }
 }
