0635de2f02fccafa618bef345bb567a44884166d
DariaSavuSoftwire <Daria.Savu@softwire.com>
2025-10-21 13:26:02 +0300

    Refactoring R2

--- Chessington.GameEngine/Pieces/Pawn.cs
+++ Chessington.GameEngine/Pieces/Pawn.cs
@@ -5,27 +5,24 @@ namespace Chessington.GameEngine.Pieces
     public class Pawn : Piece
     {
         public Pawn(Player player)
-            : base(player)
-        {
-        }
+            : base(player) { }
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
             List<Square> moves = new List<Square>();
             Square currentSquare = board.FindPiece(this);
-            Square nextSquare = Player == Player.White
-                ? Square.At(currentSquare.Row - 1, currentSquare.Col)
-                : Square.At(currentSquare.Row + 1, currentSquare.Col);
-            if (nextSquare.Row >= 0 && nextSquare.Row < GameSettings.BoardSize && board.GetPiece(nextSquare) == null)
-                moves.Add(nextSquare);
             bool hasNotMovedBefore = Player == Player.White && currentSquare.Row == GameSettings.BoardSize - 1 ||
                                      Player == Player.Black && currentSquare.Row == 1;
-            Square initialMoveUp = Player == Player.White
-                ? Square.At(currentSquare.Row - 2, currentSquare.Col)
-                : Square.At(currentSquare.Row + 2, currentSquare.Col);
-            if (hasNotMovedBefore && initialMoveUp.Row >= 0 && initialMoveUp.Row < GameSettings.BoardSize &&
-                board.GetPiece(initialMoveUp) == null)
-                moves.Add(initialMoveUp);
+            for (int move = 1; move <= 2; move++)
+            {
+                Square nextSquare = Player == Player.White
+                    ? Square.At(currentSquare.Row - move, currentSquare.Col)
+                    : Square.At(currentSquare.Row + move, currentSquare.Col);
+                if ((hasNotMovedBefore || move == 1) && nextSquare.Row >= 0 &&
+                    nextSquare.Row < GameSettings.BoardSize && board.GetPiece(nextSquare) == null)
+                    moves.Add(nextSquare);
+            }
+
             return moves;
         }
     }
