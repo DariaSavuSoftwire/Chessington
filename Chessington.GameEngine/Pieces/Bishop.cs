69f0dc521db8e659c0801dfd65821e625ce97b87
DariaSavuSoftwire <Daria.Savu@softwire.com>
2025-10-21 16:28:13 +0300

    Clean up bishop moves

--- Chessington.GameEngine/Pieces/Bishop.cs
+++ Chessington.GameEngine/Pieces/Bishop.cs
@@ -5,25 +5,19 @@ namespace Chessington.GameEngine.Pieces
 {
     public class Bishop : Piece
     {
+         private (int , int)[] bishopMoves = { (-1, -1), (1, -1), (-1, 1), (1, 1)};
         public Bishop(Player player)
             : base(player) { }
 
-        private bool IsPositionValid(int row, int col)
-        {
-            return row >= 0 && col >= 0 && row < GameSettings.BoardSize && col < GameSettings.BoardSize;
-        }
-
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            int[] directionsRow = { -1, 1, -1, 1 };
-            int[] directionsCol = { -1, -1, 1, 1 };
             List<Square> moves = new List<Square>();
             Square currentSquare = board.FindPiece(this);
-            for (int i = 0; i < 4; i++)
+            foreach(var (directionsRow, directionsCol) in bishopMoves)
             {
-                for (int row = currentSquare.Row, col = currentSquare.Col;
-                     IsPositionValid(row, col);
-                     row += directionsRow[i], col += directionsCol[i])
+                for (int row = currentSquare.Row + directionsRow, col = currentSquare.Col + directionsCol;
+                     board.IsSquareInBoard(Square.At(row, col));
+                     row += directionsRow, col += directionsCol)
                 {
                     Square nextSquare = Square.At(row, col);
                     if (board.GetPiece(nextSquare) == null)
