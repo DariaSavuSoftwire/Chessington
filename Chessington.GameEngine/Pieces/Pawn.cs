5d99c659be1500a3b6c8b8ffee6a972091a921e9
DariaSavuSoftwire <Daria.Savu@softwire.com>
2025-10-21 14:28:46 +0300

    Add special pawn check

--- Chessington.GameEngine/Pieces/Pawn.cs
+++ Chessington.GameEngine/Pieces/Pawn.cs
@@ -5,17 +5,31 @@ namespace Chessington.GameEngine.Pieces
     public class Pawn : Piece
     {
         public Pawn(Player player)
-            : base(player) { }
+            : base(player)
+        {
+        }
+
+        public int GetStartingRow(Player player)
+        {
+            return player == Player.White ? GameSettings.BoardSize - 1 : 1;
+        }
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
             List<Square> moves = new List<Square>();
             Square currentSquare = board.FindPiece(this);
-            Square nextSquare = Player == Player.White
-                ? Square.At(currentSquare.Row - 1, currentSquare.Col)
-                : Square.At(currentSquare.Row + 1, currentSquare.Col);
-            if (board.IsSquareInBoard(nextSquare) && board.GetPiece(nextSquare) == null)
-                moves.Add(nextSquare);
+            bool hasNotMovedBefore = currentSquare.Row == GetStartingRow(Player);
+            for (int move = 1; move <= 2; move++)
+            {
+                Square nextSquare = Player == Player.White
+                    ? Square.At(currentSquare.Row - move, currentSquare.Col)
+                    : Square.At(currentSquare.Row + move, currentSquare.Col);
+                if (board.IsSquareInBoard(nextSquare) && board.GetPiece(nextSquare) != null)
+                    break;
+                if ((hasNotMovedBefore || move == 1) && board.IsSquareInBoard(nextSquare))
+                    moves.Add(nextSquare);
+            }
+
             return moves;
         }
     }
