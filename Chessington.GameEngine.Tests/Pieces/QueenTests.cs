89e73b95c1a61ed25b9b9bb0e3522b7bf1d10ead
DariaSavuSoftwire <Daria.Savu@softwire.com>
2025-10-21 17:20:15 +0300

    Combined Tests

--- Chessington.GameEngine.Tests/Pieces/QueenTests.cs
+++ Chessington.GameEngine.Tests/Pieces/QueenTests.cs
@@ -9,7 +9,7 @@ namespace Chessington.GameEngine.Tests.Pieces
     public class QueenTests
     {
         [Test]
-        public void Queen_CanMove_Laterally()
+        public void Queen_CanMove_Laterally_And_Diagonally()
         {
             var board = new Board();
             var queen = new Queen(Player.White);
@@ -18,7 +18,7 @@ namespace Chessington.GameEngine.Tests.Pieces
             var moves = queen.GetAvailableMoves(board);
             var expectedMoves = new List<Square>();
             
-            foreach (var (directionsRow, directionsCol) in GameSettings.RookMoves)
+            foreach (var (directionsRow, directionsCol) in GameSettings.QueenMoves)
             {
                 for (int row = 4 + directionsRow, col = 4 + directionsCol;
                      board.IsSquareInBoard(Square.At(row, col));
@@ -29,33 +29,9 @@ namespace Chessington.GameEngine.Tests.Pieces
                         expectedMoves.Add(nextSquare);
                 }
             }
-            moves.Should().Contain(expectedMoves);
+            moves.ShouldAllBeEquivalentTo(expectedMoves);
         }
-
-        [Test]
-        public void Queen_CanMove_Diagonally()
-        {
-            var board = new Board();
-            var queen = new Queen(Player.White);
-            board.AddPiece(Square.At(4, 4), queen);
-
-            var moves = queen.GetAvailableMoves(board);
-            var expectedMoves = new List<Square>();
-            foreach (var (directionsRow, directionsCol) in GameSettings.BishopMoves)
-            {
-                for (int row = 4 + directionsRow, col = 4 + directionsCol;
-                     board.IsSquareInBoard(Square.At(row, col));
-                     row += directionsRow, col += directionsCol)
-                {
-                    Square nextSquare = Square.At(row, col);
-                    if (board.GetPiece(nextSquare) == null)
-                        expectedMoves.Add(nextSquare);
-                }
-            }
-            
-            moves.Should().Contain(expectedMoves);
-        }
-
+        
         [Test]
         public void Queen_CannotMakeAnyOtherMoves()
         {
