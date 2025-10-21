f9205bd376d2e894bc9b95e5cec918e2e19fa550
DariaSavuSoftwire <Daria.Savu@softwire.com>
2025-10-21 14:41:37 +0300

    Update Bishop movement

--- Chessington.GameEngine/Pieces/Bishop.cs
+++ Chessington.GameEngine/Pieces/Bishop.cs
@@ -7,7 +7,7 @@ namespace Chessington.GameEngine.Pieces
     {
         public Bishop(Player player)
             : base(player) { }
-        
+
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
             int[] directionsRow = { -1, 1, -1, 1 };
@@ -16,13 +16,14 @@ namespace Chessington.GameEngine.Pieces
             Square currentSquare = board.FindPiece(this);
             for (int i = 0; i < 4; i++)
             {
-                for (int row = currentSquare.Row, col = currentSquare.Col;
-                     board.IsSquareInBoard(Square.At(row,col));
+                for (int row = currentSquare.Row + directionsRow[i], col = currentSquare.Col + directionsCol[i];
+                     board.IsSquareInBoard(Square.At(row, col));
                      row += directionsRow[i], col += directionsCol[i])
                 {
                     Square nextSquare = Square.At(row, col);
-                    if (board.GetPiece(nextSquare) == null)
-                        moves.Add(nextSquare);
+                    if (board.GetPiece(nextSquare) != null)
+                        break;
+                    moves.Add(nextSquare);
                 }
             }
 
