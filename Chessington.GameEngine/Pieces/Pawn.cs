d7bcfec8f6d8b7dc2f581d1f3d9310647e087802
DariaSavuSoftwire <Daria.Savu@softwire.com>
2025-10-21 12:21:42 +0300

    Add basic pawn test

--- Chessington.GameEngine/Pieces/Pawn.cs
+++ Chessington.GameEngine/Pieces/Pawn.cs
@@ -1,16 +1,27 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 
 namespace Chessington.GameEngine.Pieces
 {
     public class Pawn : Piece
     {
-        public Pawn(Player player) 
-            : base(player) { }
+        public Pawn(Player player)
+            : base(player)
+        {
+        }
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            List<Square> moves = new List<Square>();
+            Square currentSquare = board.FindPiece(this);
+            Square nextSquare = Player == Player.White
+                ? Square.At(currentSquare.Row - 1, currentSquare.Col)
+                : Square.At(currentSquare.Row + 1, currentSquare.Col);
+            if (board.GetPiece(nextSquare) == null)
+                moves.Add(nextSquare);
+            return moves;
         }
     }
 }
